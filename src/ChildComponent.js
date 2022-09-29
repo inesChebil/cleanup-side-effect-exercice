f5ba8e1a21b6b45bdc948e53b780ece998f1c7c1
ines Chebil <ines.chebil26@gmail.com>
2022-09-29 21:38:24 +0200

    attempt1

---
+++ src/ChildComponent.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+
+function ChildComponent() {
+  const [color, setColor] = useState("red");
+
+  useEffect(() => {
+    const timer = setTimeout(() => setColor("green"), 3000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+  // useEffect(() => {
+  //   let unmounted = false;
+
+  //   setTimeout(() => {
+  //     if (!unmounted) {
+  //       setColor("green");
+  //     }
+  //   }, 3000);
+
+  //   return () => {
+  //     unmounted = true;
+  //   };
+  // }, []);
+
+  return <p style={{ color }}>{color}</p>;
+}
+export default ChildComponent;
